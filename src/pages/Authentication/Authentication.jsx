f28302723fc9c7d5b84370af1f4e924344b6a30e
ioshubham <shubham8755175722@gmail.com>
2024-03-28 18:06:13 +0530

    Login page creation

---
+++ src/pages/Authentication/Authentication.jsx
@@ -0,0 +1,43 @@
+import { Card, Grid } from '@mui/material'
+import React from 'react'
+import Login from './Login'
+
+
+const Authentication = () => {
+  return (
+    <div>
+        <Grid container>
+        <Grid className='image-container' item xs={7}>
+        <img
+          style={{
+            height: '100vh', // Set height to 100% of viewport height
+            width: 'auto', // Maintain aspect ratio
+            display: 'block', // Remove extra space below image
+            margin: '0 auto' // Center horizontally
+          }}
+          src="https://www.goodfreephotos.com/albums/vector-images/people-connected-in-a-web-vector-clipart.png"
+          alt=""
+        />
+      </Grid>
+
+      <Grid item xs={5}>
+
+          <div className='px-20 flex flex-col justify-center h-full'>
+            <Card className='card p-8 center'>
+                <div className='flex flex-col items-center mb-5 space-y-1'>
+                <h1 className='logo text-center'>Pixa Gram</h1>
+                <p className='text-center text-sm w-[70%]'>Connecting lives, Sharing Stories: Your World Your Way</p>
+                </div>
+
+                <Login></Login>
+            </Card>
+          </div>
+
+      </Grid>
+    </Grid>
+
+    </div>
+  )
+}
+
+export default Authentication
---
+++ src/pages/Authentication/Login.jsx
@@ -0,0 +1,53 @@
+import { TextField } from '@mui/material';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import React from 'react';
+import * as Yup from "yup";
+
+const initialValues = { email: "", password: "" };
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("Invalid email").required("Email is Required!"),
+  password: Yup.string().min(6,"Password must be at least 6 characters").required("Password is Required!")
+});
+
+const Login = () => {
+  // const [formValues,setFormValue] = useState();
+  const handleSubmit = (values) => {
+    console.log("handle submit", values);
+  };
+
+  return (
+    <div>
+      <Formik 
+        onSubmit={handleSubmit} 
+        validationSchema={validationSchema} 
+        initialValues={initialValues}
+      >
+        <Form className='space-y-5'>
+          <div className='space-y-5'>
+            <div>
+              <Field
+                as={TextField} 
+                name="email" 
+                placeholder="Email" 
+                type="email" 
+              />
+              <ErrorMessage name="email" component={"div"} className='text-red-500'/>
+            </div>
+            <div>
+              <Field
+                as={TextField} 
+                name="password" 
+                placeholder="Password" 
+                type="password" 
+              />
+              <ErrorMessage name="password" component={"div"} className='text-red-500'/>
+            </div>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default Login;
