d8f8fb32f158d2198e6000923a1bf8756e595554
ioshubham <shubham8755175722@gmail.com>
2024-04-04 21:14:20 +0530

    Chat & message action type defined

---
+++ src/Redux/message/message.action.js
@@ -0,0 +1,57 @@
+import { api } from "../../Config/api"
+import { CREATE_CHAT_FAILURE, CREATE_CHAT_REQUEST, CREATE_CHAT_SUCCESS, CREATE_MESSAGE_REQUEST, CREATE_MESSAGE_SUCCESS, GET_ALL_CHAT_FAILURE, GET_ALL_CHAT_REQUEST, GET_ALL_CHAT_SUCCESS } from "./message.action.type"
+
+export const createMessage=(message)=>async(dispatch)=>{
+    dispatch({type:CREATE_MESSAGE_REQUEST})
+
+    try {
+        const {data} = await api.post(`/api/messages/chat/${chatId}`,message);
+        console.log("created message",data)
+
+        dispatch({type:CREATE_MESSAGE_SUCCESS,payload:data})
+        
+    } catch (error) {
+
+        console.log("message error",error)
+        dispatch({type:CREATE_MESSAGE_FAILURE,payload:error})
+        
+    }
+
+}
+
+export const createChat=(message)=>async(dispatch)=>{
+    dispatch({type:CREATE_CHAT_REQUEST})
+
+    try {
+        const {data} = await api.post(`/api/chat`,message);
+        console.log("created chat",data)
+
+        dispatch({type:CREATE_CHAT_SUCCESS,payload:data})
+        
+    } catch (error) {
+
+        console.log("message error",error)
+        dispatch({type:CREATE_CHAT_FAILURE,payload:error})
+        
+    }
+
+}
+
+export const getAllChat=(message)=>async(dispatch)=>{
+    dispatch({type:GET_ALL_CHAT_REQUEST})
+
+    try {
+        const {data} = await api.get(`/api/chat`,message);
+        console.log("get all chats",data)
+
+        dispatch({type:GET_ALL_CHAT_SUCCESS,payload:data})
+        
+    } catch (error) {
+
+        console.log("message error",error)
+        dispatch({type:GET_ALL_CHAT_FAILURE,payload:error})
+        
+    }
+
+}
+
